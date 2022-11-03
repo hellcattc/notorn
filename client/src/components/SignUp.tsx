0a0bc753362db25830f9115035fb48930076c391
hellcattc <slhtte345@gmail.com>
2022-11-03 14:05:26 +0300

    Added client

---
+++ client/src/components/SignUp.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SignUp = () => {
+
+    const handleUserSignUp = () => {
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleUserSignUp}>
+                <input></input>
+                <input></input>
+                <input></input>
+                <button type='submit'>Sign Up</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp
